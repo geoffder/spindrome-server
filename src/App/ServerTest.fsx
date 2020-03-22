25798c2871373202070da9c10fd331f417fce6e3
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-03-22 01:43:03 -0700

    Comment out unsuccessfull (?) fsx script fsac fix.

--- src/App/ServerTest.fsx
+++ src/App/ServerTest.fsx
@@ -1,3 +1,4 @@
+// #r "/usr/share/dotnet/sdk/3.1.102/ref/netstandard.dll"  // for fsac?
 #r "/home/geoff/.nuget/packages/suave/2.5.6/lib/netstandard2.0/Suave.dll"
 #r "/home/geoff/.nuget/packages/websocketsharp.standard/1.0.3/lib/netstandard2.0/websocket-sharp-standard.dll"
 #r "/home/geoff/.nuget/packages/newtonsoft.json/12.0.3/lib/netstandard2.0/Newtonsoft.Json.dll"
